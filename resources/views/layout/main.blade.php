10badb47c773da3088c3fa811d1cab8e6a0c9675
carlo-ddm <carloduminuco90@gmail.com>
2022-06-30 17:59:13 +0200

    creazione layout / creazione PageController / impostazione della rotta statica

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-base-crud</title>
+</head>
+<body>
+{{-- Creare un nuovo progetto Laravel 7 e tramite gli appositi comandi artisan create un model con relativa migration e un resource controller.
+Creare il seeder per la tabella comics utilizzando il file in allegato.
+Creare la pagina index e la pagina show
+BONUS: impostare la pagina create contenete il form per inviare i dati in POST allo store --}}
+    @yield('content')
+</body>
+</html>
