b53001badc95ac02ff805824b9701afa342bb5ff
carlo-ddm <carloduminuco90@gmail.com>
2022-07-04 15:49:54 +0200

    esercizio completo

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -95,6 +95,23 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
+        $request->validate([
+            'title' => 'required|min:3|max:50',
+            'image' => 'required|min:10|max:255',
+            'type' => 'required|min:3|max:50'
+        ],
+        [
+            'title.required'  => 'Campo obbligatorio',
+            'title.min'  => 'Il titolo deve avere almeno :min caratteri',
+            'title.max'  => 'Il titolo non può superare i :max caratteri',
+            'image.required'  => 'Campo obbligatorio',
+            'image.min'  => 'L\'URL deve avere almeno :min caratteri',
+            'image.max'  => 'L\'URL non può superare i :max caratteri',
+            'type.required'  => 'Campo obbligatorio',
+            'type.min'  => 'La categoria deve avere almeno :min caratteri',
+            'type.max'  => 'La categoria non può superare i :max caratteri',
+        ]);
+
         $data = $request->all();
         $comic->update($data);
         return redirect()->route('comics.show', compact('comic'));
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,22 +1,40 @@
 @extends('layout.main')
 @section('content')
 <div class="container">
+    {{-- gestione errori --}}
+    <div>
+        @if ($errors->any())
+            {{-- @dump($errors->any()) --}}
+            <div class="col-8 offset-2 alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+    </div>
     <form action="{{route('comics.update', $comic)}}" method="POST">
         @method('PUT')
         {{-- csrf: Obbligatorio su tutti i form di laravel--> è un token di laravel che verifica che le info provengano da un form del sito stesso --}}
         @csrf
         <div class="form-group">
           <label for="image">Image</label>
-          <input type="text" class="form-control" name="image" id="image" value="{{$comic->image}}">
+          <input type="text" class="form-control @error('image') is-invalid @enderror" name="image" id="image"
+          placeholder="URL Immagine" value="{{old('image',$comic->image)}}">
         </div>
+
         <div class="form-group">
           <label for="title">Title</label>
-          <input type="text" class="form-control" name="title" id="title" value="{{$comic->title}}">
+          <input type="text" class="form-control @error('title') is-invalid @enderror" name="title" id="title" placeholder="URL Immagine" value="{{old('title',$comic->title)}}">
         </div>
+
         <div class="form-group">
           <label for="type">Type</label>
-          <input type="text" class="form-control" name="type" id="type" value="{{$comic->type}}">
+          <input type="text" class="form-control @error('type') is-invalid @enderror" name="type" id="type"
+          placeholder="URL Immagine" value="{{old('type',$comic->type)}}">
         </div>
+
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 </div>
